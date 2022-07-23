554a1fbc2e1689f873af57918318b171a0d546c4
TRACIE-F <99625130+TRACIE-F@users.noreply.github.com>
2022-07-23 12:27:33 -0500

    Folder re-organization
    Added resources, moved csv files, adjusted R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,10 +1,10 @@
 library(dplyr)
-mechacar_mpg <- read.csv('MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+mechacar_mpg <- read.csv('./Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg))
 
 library(tidyverse)
-sus_coil <- read.csv('Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+sus_coil <- read.csv('./Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- sus_coil %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI),Std_Dev_PSI=sd(PSI),Num_Coil=n(), .groups = 'keep')
 lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI),Std_Dev_PSI=sd(PSI),Num_Coil=n(), .groups = 'keep')
 
